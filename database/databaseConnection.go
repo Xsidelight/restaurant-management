d5d66b1128da8492e7e72011d1d44f5f145b0f9e
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2023-11-24 12:20:11 +0600

    create database connection

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,41 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDb := "mongodb://127.0.0.1:27017"
+	fmt.Print(MongoDb)
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(MongoDb).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(context.TODO(), opts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb")
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection = client.Database("restaurant").Collection(collectionName)
+	return collection
+}
