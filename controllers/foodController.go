ef3af10ff54b624f48fe8c9ef53164191af465f5
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2023-11-27 20:39:22 +0600

    add getFood function

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,33 +1,59 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Xsidelight/restaurant-management/database"
+	"github.com/Xsidelight/restaurant-management/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(c *gin.Context) {
+
+	}
 
 }
 
 func GetFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+		foodId := c.Param("food_id")
+		var food models.Food
+
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching the food item"})
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, food)
+
+	}
 
 }
 
 func CreateFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(c *gin.Context) {}
 
 }
 
-func round(num float64) int  {
-	
+func round(num float64) int {
+
 }
 
 func toFixed(num float64, precision int) float64 {
-	
-} 
-
-func UpdateFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
 
 }
 
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {}
 
+}
