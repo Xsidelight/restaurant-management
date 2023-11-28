cfb285373623b6c86d593f9f6a0f8fca4e8474e2
Xsidelight <toko.gogberaa@gmail.com>
2023-11-28 11:45:23 +0600

    add food and menu controller methods

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -2,17 +2,21 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/Xsidelight/restaurant-management/database"
 	"github.com/Xsidelight/restaurant-management/models"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var validate = validator.New()
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -41,7 +45,44 @@ func GetFood() gin.HandlerFunc {
 }
 
 func CreateFood() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+		var food models.Food
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validatorErr := validate.Struct(&food)
+		if validatorErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validatorErr.Error()})
+			return
+		}
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		if err != nil {
+			msg := fmt.Sprintf("menu with id %s not found", food.Menu_id)
+			c.JSON(http.StatusBadRequest, gin.H{"error": msg})
+			return
+		}
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.ID = primitive.NewObjectID()
+		food.Food_id = food.ID.Hex()
+		var num = toFixed(*food.Price, 2)
+		food.Price = &num
+
+		result, insertError := foodCollection.InsertOne(ctx, food)
+		if insertError != nil {
+			msg := fmt.Sprintf("food item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, result)
+
+	}
 
 }
 
