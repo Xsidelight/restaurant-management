cabbc9a0e320b8f0375871fac2a8c5e9dd77b88e
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2023-12-04 16:45:16 +0600

    add items by order function

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -82,6 +82,81 @@ func GetOrderItemsByOrder() gin.HandlerFunc {
 }
 
 func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	matchStage := bson.D{{Key: "$match", Value: bson.D{{Key: "order_id", Value: id}}}}
+	lookupStage := bson.D{{Key: "$lookValue: up", Value: bson.D{{Key: "from", Value: "food"}, {Key: "localField", Value: "food_id"}, {Key: "localField", Value: "food_id"}, {Key: "as", Value: "food"}}}}
+	unwindStage := bson.D{{Key: "$unwind", Value: bson.D{{Key: "path", Value: "$food"}, {Key: "preserveNullAndEmptyArrays", Value: true}}}}
+
+	lookupOrderStage := bson.D{{Key: "$lookup", Value: bson.D{{Key: "food", Value: "order"}, {Key: "localField", Value: "order_id"}, {Key: "foreignField", Value: "order_id"}, {Key: "as", Value: "order"}}}}
+	unwindOrderStage := bson.D{{Key: "$unwind", Value: bson.D{{Key: "path", Value: "$order"}, {Key: "preserveNullAndEmptyArrays", Value: true}}}}
+
+	lookupTableStage := bson.D{{Key: "$lookup", Value: bson.D{{Key: "from", Value: "table"}, {Key: "localField", Value: "table_id"}, {Key: "foreignField", Value: "table_id"}, {Key: "as", Value: "table"}}}}
+	unwindTableStage := bson.D{{Key: "$unwind", Value: bson.D{{Key: "path", Value: "$table"}, {Key: "preserveNullAndEmptyArrays", Value: true}}}}
+
+	projectStage := bson.D{
+		{Key: "$project", Value: bson.D{
+			{Key: "id", Value: 0},
+			{Key: "amount", Value: "$food.price"},
+			{Key: "total_count", Value: 1},
+			{Key: "food_name", Value: "$food.name"},
+			{Key: "food_image", Value: "$food.food_image"},
+			{Key: "table_number", Value: "$table.table_number"},
+			{Key: "table_id", Value: "$table.table_id"},
+			{Key: "order_id", Value: "$order.order_id"},
+			{Key: "price", Value: "$food.price"},
+			{Key: "quantity", Value: 1},
+		}},
+	}
+
+	groupStage := bson.D{{Key: "$group", Value: bson.D{
+		{Key: "_id", Value: bson.D{
+			{Key: "food_name", Value: "$food_name"},
+			{Key: "food_image", Value: "$food_image"},
+			{Key: "table_number", Value: "$table_number"},
+			{Key: "table_id", Value: "$table_id"},
+			{Key: "order_id", Value: "$order_id"},
+			{Key: "price", Value: "$price"},
+		}},
+		{Key: "total_count", Value: bson.D{{Key: "$sum", Value: "$total_count"}}},
+		{Key: "amount", Value: bson.D{{Key: "$sum", Value: "$amount"}}},
+		{Key: "quantity", Value: bson.D{{Key: "$sum", Value: "$quantity"}}},
+	}}}
+
+	projectStage2 := bson.D{
+		{Key: "$project", Value: bson.D{
+
+			{Key: "id", Value: 0},
+			{Key: "payment_due", Value: 1},
+			{Key: "total_count", Value: 1},
+			{Key: "table_number", Value: "$_id.table_number"},
+			{Key: "order_items", Value: 1},
+		}}}
+
+	result, err := orderItemCollection.Aggregate(ctx, mongo.Pipeline{
+		matchStage,
+		lookupStage,
+		unwindStage,
+		lookupOrderStage,
+		unwindOrderStage,
+		lookupTableStage,
+		unwindTableStage,
+		projectStage,
+		groupStage,
+		projectStage2})
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err = result.All(ctx, &OrderItems); err != nil {
+		panic(err)
+	}
+
+	defer cancel()
+
+	return OrderItems, err
 
 }
 
@@ -185,9 +260,4 @@ func UpdateOrderItem() gin.HandlerFunc {
 		c.JSON(http.StatusOK, result)
 
 	}
-}
-
-// TODO: DeleteOrderItem function
-func DeleteOrderItem() gin.HandlerFunc {
-	return func(c *gin.Context) {}
 }
